e22a90e8cc4af660f85b4b0eecdbbeee3b5ebb12
ychartois <ychartois@gmail.com>
2025-02-26 11:01:24 +0000

    eat cryptoKitty

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -17,7 +17,7 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string memory _name, uint _dna) private {
+    function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
