cf00a550e759f15dfa21b3a44013f5ef8baecc0a
ychartois <ychartois@gmail.com>
2025-03-05 15:14:11 +0000

    implements attack logic

--- zombieattack.sol
+++ zombieattack.sol
@@ -13,9 +13,22 @@ contract ZombieAttack is ZombieHelper {
     }
 
     function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
-       Zombie storage myZombie = zombies[_zombieId];
-       Zombie storage enemyZombie = zombies[_targetId];
-       uint rand = randMod(100);
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+        uint rand = randMod(100);
+
+        if (rand <= attackVictoryProbability) {
+            myZombie.winCount++;
+            myZombie.level++;
+            enemyZombie.lossCount++;
+
+            feedAndMultiply(_zombieId, myZombie.dna, "zombie");
+        } else {
+            myZombie.lossCount++;
+            enemyZombie.winCount++;
+            _triggerCooldown(myZombie);
+        }
+
     }
 
 }
--- zombiefactory.sol
+++ zombiefactory.sol
@@ -15,6 +15,8 @@ contract ZombieFactory is Ownable {
         uint dna;
         uint32 level;
         uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
     }
 
     mapping (uint => address) public zombieToOwner ;
@@ -23,7 +25,7 @@ contract ZombieFactory is Ownable {
     Zombie[] public zombies;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
